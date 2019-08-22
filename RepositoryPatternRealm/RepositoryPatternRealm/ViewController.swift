5d3cc051d53acf8663d9a1837b21b381fe5786d8
Kristijan Kralj <kristijan.kralj@live.com>
2019-08-22 13:25:34 +0200

    Create project

---
+++ RepositoryPatternRealm/RepositoryPatternRealm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RepositoryPatternRealm
+//
+//  Created by Kristijan Kralj on 22/08/2019.
+//  Copyright © 2019 Kristijan Kralj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
